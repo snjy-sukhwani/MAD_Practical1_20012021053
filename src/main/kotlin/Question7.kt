811e6b66d454085b3f34be3f1dddfabeae644dae
Sanjay Sukhwani <sanjaysukhwani20@gnu.ac.in>
2022-08-19 14:03:32 +0530

    Adding Coding File . . .

---
+++ src/main/kotlin/Question7.kt
@@ -0,0 +1,24 @@
+// Question : Find the factorial of number by recursion. Explain "tailrec" keyword.
+
+// Tail Recursion :
+// In tail recursion, calculations are performed first, then recursive calls are executed
+// To tell compiler to perform tail recursion in Kotlin, we need to mark the function with tailrec modifier.
+
+fun factLoop(num:Int):Int{
+    var fact=1
+    for (i in 1..num) {
+        fact *= i
+    }
+    return fact
+}
+
+tailrec fun factTailrec(n:Int):Int {
+    return if (n==1) 1 else n * factTailrec(n-1)
+}
+
+fun main() {
+    print("Enter Number : ")
+    val num : Int = readLine()!!.toInt()
+    println("Factorial of 5 : ${factLoop(num)}")
+    println("By Tailrec Keyword, Factorial of 5 : ${factTailrec(num)}")
+}
